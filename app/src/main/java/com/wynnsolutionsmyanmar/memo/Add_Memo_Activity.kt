5780461e4192a56521b19989831395a816cf160b
Hein Thura Wynn <heinthurawynn.developer@gmail.com>
2019-07-16 22:45:52 +0630

    Almost all are done but logical err still there

--- app/src/main/java/com/wynnsolutionsmyanmar/memo/Add_Memo_Activity.kt
+++ app/src/main/java/com/wynnsolutionsmyanmar/memo/Add_Memo_Activity.kt
@@ -3,18 +3,33 @@ package com.wynnsolutionsmyanmar.memo
 import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add__memo.*
 import android.view.View
+import android.widget.CompoundButton
+import android.widget.Switch
+import java.security.MessageDigest
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
 
 class Add_Memo_Activity : AppCompatActivity() {
+
     val dbTable="Notes"
     var id=0
+    val AES = "AES"
+    var encTitle: String = ""
+    var encDesc: String = ""
+    var decTitle: String = ""
+    var decDesc: String = ""
+    var enc_dec: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add__memo)
 
-       try{
+        try{
             var bundle:Bundle= intent.extras!!
             id=bundle.getInt("ID",0)
             if(id!=0) {
@@ -24,33 +39,37 @@ class Add_Memo_Activity : AppCompatActivity() {
             }
         }catch (ex:Exception){}
 
+//        enc_decToggle.setOnCheckedChangeListener(CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+//            if (isChecked) {
+//                val sharedPreference = SharedPreference(this)
+//                val password = sharedPreference.getValueString("code")
+//                encTitle = encrypt(etTitle.text.toString(), password.toString())
+//                encDesc  = encrypt(etDes.text.toString(),password.toString())
+//                etTitle.setText(encTitle)
+//                etDes.setText(encDesc)
+//                enc_dec = true
+//                Toast.makeText(this,"Encrypted",Toast.LENGTH_LONG).show()
+//            } else {
+//                val sharedPreference = SharedPreference(this)
+//                val password = sharedPreference.getValueString("code")
+//                decTitle = decrypt(etTitle.text.toString(), password.toString())
+//                decDesc  = decrypt(etDes.text.toString(),password.toString())
+//                etTitle.setText(decTitle)
+//                etDes.setText(decDesc)
+//                enc_dec = false
+//                Toast.makeText(this,"Decrypted",Toast.LENGTH_LONG).show()
+//
+//            }
+//        })
+
 
     }
 
     fun btnAdd(view:View){
         var dbManager= DbManager(this)
-
         var values= ContentValues()
         values.put("Title",etTitle.text.toString())
         values.put("Description",etDes.text.toString())
-//         // Get the switch button state programmatically
-//         if(btnEncDec.isChecked){
-//             // If switch button is checked/on then
-//             // The switch is enabled/checked
-//             values.put("Title",etTitle.text.toString())
-//             values.put("Description",etDes.text.toString())
-//             values.put("Date",calendarView.date.toString())
-//             values.put("Status","Encrypt")
-
-
-//         }else{
-//             // The switch is unchecked
-//             values.put("Title",etTitle.text.toString())
-//             values.put("Description",etDes.text.toString())
-//             values.put("Date",calendarView.date.toString())
-//             values.put("Status","Decrypt")
-//         }
-
         if(id==0) {
             val ID = dbManager.Insert(values)
             if (ID > 0) {
@@ -70,5 +89,56 @@ class Add_Memo_Activity : AppCompatActivity() {
             }
         }
 
+    }
+
+    fun btnEnc(view: View){
+        val sharedPreference = SharedPreference(this)
+        val password = sharedPreference.getValueString("code")
+        encTitle = encrypt(etTitle.text.toString(), password.toString())
+        encDesc  = encrypt(etDes.text.toString(),password.toString())
+        etTitle.setText(encTitle)
+        etDes.setText(encDesc)
+        enc_dec = true
+        Toast.makeText(this,"Encrypted",Toast.LENGTH_LONG).show()
+    }
+    fun btnDec(view: View){
+
+        val sharedPreference = SharedPreference(this)
+        val password = sharedPreference.getValueString("code")
+        decTitle = decrypt(etTitle.text.toString(), password.toString())
+        decDesc  = decrypt(etDes.text.toString(),password.toString())
+        etTitle.setText(decTitle)
+        etDes.setText(decDesc)
+        enc_dec = false
+        Toast.makeText(this,"Decrypted",Toast.LENGTH_LONG).show()
+
+    }
+
+    @Throws(Exception::class)
+    private fun decrypt(outputString: String, password: String): String {
+        val key = generateKey(password)
+        val c = Cipher.getInstance(AES)
+        c.init(Cipher.DECRYPT_MODE, key)
+        val decodedValue = Base64.decode(outputString, Base64.DEFAULT)
+        val decValue = c.doFinal(decodedValue)
+        return String(decValue)
+    }
+
+    @Throws(Exception::class)
+    private fun encrypt(Data: String, password: String): String {
+        val key = generateKey(password)
+        val c = Cipher.getInstance(AES)
+        c.init(Cipher.ENCRYPT_MODE, key)
+        val encVal = c.doFinal(Data.toByteArray())
+        return Base64.encodeToString(encVal, Base64.DEFAULT)
+    }
+
+    @Throws(Exception::class)
+    private fun generateKey(password: String): SecretKeySpec {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val bytes = password.toByteArray(charset("UTF-8"))
+        digest.update(bytes, 0, bytes.size)
+        val key = digest.digest()
+        return SecretKeySpec(key, "AES")
     }
 }
